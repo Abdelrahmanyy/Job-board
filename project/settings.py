2854d38bdf01292849b1b26021c7226cda7377fe
Abdelrahmanyy <diefabdo9@gmail.com>
2023-09-07 02:56:53 +0200

    contact app

--- project/settings.py
+++ project/settings.py
@@ -144,4 +144,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
 
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'email@gmail.com'
+EMAIL_HOST_PASSWORD = 'Key'
+EMAIL_USE_TLS = True
+EMAIL_PORT = '587'
